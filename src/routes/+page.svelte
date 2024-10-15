194a6a23a5e4aaf417cab493cdb45ea147cd5251
Pinxuan Huang <pinxhuang@gmail.com>
2024-10-15 15:45:13 -0400

    Skeleton game

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Game from "$lib/components/game/Game.svelte";
+</script>
+
+<h1>fruitale</h1>
+<Game />
